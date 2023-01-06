74cd5a45cc55f08a94642e1d341e22bfa26cfde7
porrapat <iporrapat@gmail.com>
2023-01-06 15:44:59 +0700

    lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    string grade[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    int k=rand()%9;
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0;i<3;i++)
+    {
+        cin.get();
+    }
+    string g=grade[k];
+    cout << "You will get "<< g << " in this 261102. ";
+}
